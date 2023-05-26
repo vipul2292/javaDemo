c55de3a1926ea5c19fa4a2549a2f171d24aa70f6
vipul2292 <vipulchoubey2292@gmail.com>
2023-05-26 17:53:59 +0530

    commit by d1

--- pw.java
+++ pw.java
@@ -1,6 +1,7 @@
 package Demo;
 class Main{
 	public static void main (String args[]) {
-		System.out.println("Hello"); 
+		System.out.println("Hello");
+		System.out.println("I am developer1 or D1"); 
 	}
 }
